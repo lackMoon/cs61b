47909cbe020a953ef2f282a221a910c0e064eee6
lackMoon <fox_zichen@foxmail.com>
2023-01-25 23:05:15 +0900

    proj1ec commit

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -16,25 +16,30 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> std = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
         List<String> callSequence = new ArrayList<>();
-        for (int i = 0; i < 50; i += 1) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.25) {
+        for (int i = 0; i < 100; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform(100);
+            if (numberBetweenZeroAndOne < 20) {
                 std.addFirst(i);
                 ad.addFirst(i);
                 callSequence.add("addFirst(" + i + ")");
-            } else if (numberBetweenZeroAndOne >= 0.25 && numberBetweenZeroAndOne < 0.5) {
+            } else if (numberBetweenZeroAndOne >= 20 && numberBetweenZeroAndOne < 40) {
                 std.addLast(i);
                 ad.addLast(i);
                 callSequence.add("addLast(" + i + ")");
-            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
+            } else if (numberBetweenZeroAndOne >= 40 && numberBetweenZeroAndOne < 60) {
                 Integer sv = std.isEmpty() ? null : std.removeFirst();
                 Integer av = ad.isEmpty() ? null : ad.removeFirst();
                 callSequence.add("removeFirst()");
                 assertEquals(String.join("\n", callSequence), av, sv);
-            } else {
+            } else if (numberBetweenZeroAndOne >= 60 && numberBetweenZeroAndOne < 80) {
                 Integer sv = std.isEmpty() ? null : std.removeLast();
                 Integer av = ad.isEmpty() ? null : ad.removeLast();
                 callSequence.add("removeLast()");
+                assertEquals(String.join("\n", callSequence), av, sv);
+            } else {
+                Integer sv = std.size();
+                Integer av = ad.size();
+                callSequence.add("size()");
                 assertEquals(String.join("\n", callSequence), av, sv);
             }
         }
