bf28043ea9b76ae61a1eb9598ad67a73fcaba0c6
lackMoon <fox_zichen@foxmail.com>
2023-01-29 14:44:11 +0900

    buggy version

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,26 @@
  */
 public class Collatz {
 
-    /** implementation of nextNumber! */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  % 2 == 0) {
-            return n / 2;
-        } else {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
+
+
+    /** implementation of nextNumber! */
+    //public static int nextNumber(int n) {
+    //    if (n  % 2 == 0) {
+    //        return n / 2;
+    //    } else {
+    //        return 3 * n + 1;
+   //    }
+   // }
 
     public static void main(String[] args) {
         int n = 5;
